c838787d592ce5399ea863578fa21d9d9247a872
Shakil57375 <shakil57375@gmail.com>
2024-02-05 21:33:03 +0600

    data added

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -1,16 +1,25 @@
 const Hero = () => {
   return (
-    <div className=" py-24 ">
-      <div className=" flex items-center justify-between mx-40">
+    <div className=" py-16">
+      <div className=" flex items-center justify-between px-40">
         <div className="lg:basis-1/2 basis-full">
-          <h1 className="text-4xl md:text-6xl font-bold mb-4">Welcome to Your E-Commerce Store</h1>
-          <p className="text-lg md:text-xl mb-8">Discover amazing products just for you</p>
-          <p className="text-sm md:text-base mb-8">Shop from a wide range of categories and find the perfect items for you.</p>
-          <button className="bg-white text-blue-500 px-6 py-3 rounded-full font-bold">Shop Now</button>
+          <h1 className="text-4xl md:text-6xl font-bold mb-4">
+            Welcome to Your E-Commerce Store
+          </h1>
+          <p className="text-lg md:text-xl mb-8">
+            Discover amazing products just for you
+          </p>
+          <p className="text-sm md:text-base mb-8">
+            Shop from a wide range of categories and find the perfect items for
+            you.
+          </p>
+          <button className="bg-gradient-to-r from-red-500 to-orange-500 px-6 py-3 !rounded-full hover:bg-gradient-to-l hover:to-orange-500 hover:from-red-500 font-bold text-white transition-all duration-500 hover:-translate-x-2 hover:-translate-y-2">
+            Shop Now
+          </button>
         </div>
         <div className="lg:basis-1/2 basis-full">
           <img
-            className="w-[500px] h-[500px] md:rounded-lg"
+            className="w-full h-[600px] md:rounded-lg"
             src="https://i.ibb.co/YT4WpdL/online-fashion-shopping.png"
             alt="Hero Image"
           />
--- src/pages/Trending/Trending.jsx
+++ src/pages/Trending/Trending.jsx
@@ -1,9 +1,17 @@
+import ProductCard from "../../components/ProductCard/ProductCard";
+import  products  from "../../database/data.json";
 const Trending = () => {
+  console.log(products);
   return (
     <div>
-      <p>Trending</p>
+      <p className="text-center text-4xl font-bold my-4">Trending Products</p>
+      {
+        products.map(product =>(
+          <ProductCard key={product.id} product = {product} />
+        ))
+      }
     </div>
-  )
-}
+  );
+};
 
-export default Trending
+export default Trending;
